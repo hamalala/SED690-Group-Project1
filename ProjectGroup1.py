113ab98f2789dcec95f176beb0e953cd2e731249
hamalala <46687725+hamalala@users.noreply.github.com>
2024-08-31 22:28:26 +0700

    Update ProjectGroup1.py

--- ProjectGroup1.py
+++ ProjectGroup1.py
@@ -9,7 +9,7 @@ def predict(rules, input_items):
 
 # Set title
 st.title("Discovering Frequent Patterns and Association Rules")
-
+st.write("Group: 704-710-712-714")
 st.markdown('[Can edit this sheet for your transaction](https://docs.google.com/spreadsheets/d/1-h4q2swBlPGO76pJkExdtIoVoMyoePjkovyk71c-PzA)')
 # Define the radio button options
 options = ['Apriori Algorithm', 'FP-Growth Algorithm']
