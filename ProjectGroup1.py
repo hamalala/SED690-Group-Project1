b7d1b2445b3949980d0a8a77e99553f9fd37c18b
hamalala <46687725+hamalala@users.noreply.github.com>
2024-08-31 21:32:07 +0700

    Create ProjectGroup1.py

---
+++ ProjectGroup1.py
@@ -0,0 +1,48 @@
+
+import streamlit as st
+
+# Set title
+st.title("Discovering Frequent Patterns and Association Rules")
+
+st.markdown('[Can edit this sheet for you transaction](https://docs.google.com/spreadsheets/d/1-h4q2swBlPGO76pJkExdtIoVoMyoePjkovyk71c-PzA)')
+# Define the radio button options
+options = ['Apriori Algorithm', 'FP-Growth Algorithm']
+
+# Create a radio button widget
+algorithm = st.radio("Choose an Algorithm:", options)
+
+# Creating a range slider
+min_support = st.slider(
+    "Select minimum Support:",
+    min_value=2,  # Minimum value of the slider
+    max_value=20,  # Maximum value of the slider
+    value=3,  # Default value of the slider
+    step=1  # Step size between values
+)
+
+# Creating a range slider
+min_confidence = st.slider(
+    "Select minimum Confidence:",
+    min_value=0,  # Minimum value of the slider
+    max_value=1,  # Maximum value of the slider
+    value=0.5,  # Default value of the slider
+    step=0.05  # Step size between values
+)
+
+
+# Creating a range slider
+min_lift = st.slider(
+    "Select minimum Lift:",
+    min_value=0,  # Minimum value of the slider
+    max_value=10,  # Maximum value of the slider
+    value=1,  # Default value of the slider
+    step=0.1  # Step size between values
+)
+
+
+
+# Display prediction result
+st.write("## Prediction Result: {algorithm}")
+st.write("## Prediction min_support: {min_support}")
+st.write("## Prediction min_confidence: {min_confidence}")
+st.write("## Prediction min_lift: {min_lift}")
