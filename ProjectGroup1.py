0119cd05c63b178103326abe9eda80f233377557
hamalala <46687725+hamalala@users.noreply.github.com>
2024-08-31 21:35:39 +0700

    Update ProjectGroup1.py

--- ProjectGroup1.py
+++ ProjectGroup1.py
@@ -7,7 +7,6 @@ st.title("Discovering Frequent Patterns and Association Rules")
 st.markdown('[Can edit this sheet for you transaction](https://docs.google.com/spreadsheets/d/1-h4q2swBlPGO76pJkExdtIoVoMyoePjkovyk71c-PzA)')
 # Define the radio button options
 options = ['Apriori Algorithm', 'FP-Growth Algorithm']
-
 # Create a radio button widget
 algorithm = st.radio("Choose an Algorithm:", options)
 
@@ -23,26 +22,23 @@ min_support = st.slider(
 # Creating a range slider
 min_confidence = st.slider(
     "Select minimum Confidence:",
-    min_value=0,  # Minimum value of the slider
-    max_value=1,  # Maximum value of the slider
+    min_value=0.0,  # Minimum value of the slider
+    max_value=1.0,  # Maximum value of the slider
     value=0.5,  # Default value of the slider
     step=0.05  # Step size between values
 )
 
-
 # Creating a range slider
 min_lift = st.slider(
     "Select minimum Lift:",
-    min_value=0,  # Minimum value of the slider
-    max_value=10,  # Maximum value of the slider
-    value=1,  # Default value of the slider
+    min_value=0.0,  # Minimum value of the slider
+    max_value=10.0,  # Maximum value of the slider
+    value=1.0,  # Default value of the slider
     step=0.1  # Step size between values
 )
 
-
-
 # Display prediction result
-st.write("## Prediction Result: {algorithm}")
-st.write("## Prediction min_support: {min_support}")
-st.write("## Prediction min_confidence: {min_confidence}")
-st.write("## Prediction min_lift: {min_lift}")
+st.write(f"## Prediction Result: {algorithm}")
+st.write(f"## Prediction min_support: {min_support}")
+st.write(f"## Prediction min_confidence: {min_confidence}")
+st.write(f"## Prediction min_lift: {min_lift}")
