56724336afba90884774543f59cd864283849902
hamalala <46687725+hamalala@users.noreply.github.com>
2024-09-01 09:42:51 +0700

    Update ProjectGroup1.py

--- ProjectGroup1.py
+++ ProjectGroup1.py
@@ -45,7 +45,7 @@ if st.button("Run Algorithm"):
         st.session_state['algorithm_df'] = frequent_itemsets
         
         if frequent_itemsets.empty:
-            rules = None
+            rules = pd.DataFrame()
             st.write("No frequent itemsets found with the given minimum support.")
         else:
             rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
